f361d95100bee036f364e0e4485bf1fac4904997
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-01-05 13:45:22 +0000

    Sync LeetCode submission Runtime - 1 ms (87.09%), Memory - 45.6 MB (29.27%)

---
+++ 0046-permutations/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<>();
+        perms.add(new ArrayList<>());
+        
+        for(int n: nums){
+            List<List<Integer>>  newPerms = new ArrayList<>();
+            for(List<Integer> p: perms){
+                for(int i=0; i<p.size()+1; i++){
+                    List<Integer> pCopy = new ArrayList<>(p);
+                    pCopy.add(i, n);
+                    newPerms.add(pCopy);
+                }
+            }
+            perms = newPerms;
+        }
+        return perms;
+    }
+}
