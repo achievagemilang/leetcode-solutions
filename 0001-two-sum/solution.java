2b2204e4cee6c8899628c457370b9217d6d4bc5c
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-01-06 04:37:01 +0000

    Sync LeetCode submission Runtime - 2 ms (99.30%), Memory - 47.5 MB (10.98%)

---
+++ 0001-two-sum/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> mp = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int n = target - nums[i];
+            if(mp.containsKey(n)){
+                return new int[]{mp.get(n), i};
+            }
+            mp.putIfAbsent(nums[i], i);
+        }
+
+        return new int[]{0,0};
+    }
+}
