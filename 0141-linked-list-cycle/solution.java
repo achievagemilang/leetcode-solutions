e7fa1133829c3ec25a4bac13f49d977797e7f66b
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-07-19 07:19:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (52.47%)

--- 0141-linked-list-cycle/solution.java
+++ 0141-linked-list-cycle/solution.java
@@ -1,25 +1,24 @@
 /**
  * Definition for singly-linked list.
- * class ListNode {
+ * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-public class Solution {
+
+class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null) return false;
-        ListNode fast = head, slow = head;
-        while(fast != null && fast.next != null && fast.next.next != null){
-            slow = slow.next;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
             fast = fast.next.next;
-            if(slow == fast){
-                return true;
-            }
+            slow = slow.next;
+            if(fast == slow) return true;
         }
+
         return false;
     }
 }
+
