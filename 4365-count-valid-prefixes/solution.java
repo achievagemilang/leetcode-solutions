34f2c3f6c744640d1ba6d224a4c81d09c841eaa4
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-08-01 15:24:11 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 43.8 MB (79.40%)

---
+++ 4365-count-valid-prefixes/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int zeros = 0;
+        int ones = 0;
+        int ans = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '0'){
+                zeros++;
+            } else {
+                ones++;
+            }
+
+            if(Math.abs(zeros - ones) <= 1){
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
