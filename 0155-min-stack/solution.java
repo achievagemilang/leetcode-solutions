7ed9f3b7f98317e981448d7f665f32338f7d53ae
Achieva Futura Gemilang <achievafuturagemilang@gmail.com>
2026-08-03 14:35:51 +0000

    Sync LeetCode submission Runtime - 32 ms (76.60%), Memory - 103 MB (11.77%)

---
+++ 0155-min-stack/solution.java
@@ -0,0 +1,37 @@
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+    public MinStack() {
+        this.stack = new ArrayDeque<>();
+        this.minStack = new ArrayDeque<>();
+        
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+        if(minStack.isEmpty() || minStack.peek() >= value)
+            minStack.push(value);
+    }
+    
+    public void pop() {
+        int val = stack.pop();
+        if (minStack.peek() == val) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
